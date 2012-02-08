c03610770126c15fc30c5c5a59ca7363fdc1368b
Jannis Leidel <jannis@leidel.info>
2012-02-08 20:03:56 +0100

    Added support for Postgres' UUID type.
    - This also changes the return type of the field to be `StringUUID`, a subclass of `uuid.UUID` that uses the purely hexadecimal representation when coerced to a string.
    - Sets the field's form max_length automatically to 32.
    - Use modern introspection API of South.
    - Added a bit of validation for the namespace and name parameters.
    - Extended tests.

--- uuidfield/tests/models.py
+++ uuidfield/tests/models.py
@@ -1,6 +1,19 @@
+import uuid
 from django.db import models
-
 from uuidfield import UUIDField
 
-class UUIDFieldTestModel(models.Model):
+
+class AutoUUIDField(models.Model):
     uuid = UUIDField(auto=True)
+
+
+class ManualUUIDField(models.Model):
+    uuid = UUIDField(auto=False)
+
+
+class NamespaceUUIDField(models.Model):
+    uuid = UUIDField(auto=True, namespace=uuid.NAMESPACE_URL, version=5)
+
+
+class BrokenNamespaceUUIDField(models.Model):
+    uuid = UUIDField(auto=True, namespace='lala', version=5)
--- uuidfield/tests/tests.py
+++ uuidfield/tests/tests.py
@@ -1,13 +1,38 @@
-from django.db import connection
-from django.test import TestCase
-
-from uuidfield.tests.models import UUIDFieldTestModel
-
 import uuid
 
+from django.db import connection, IntegrityError
+from django.test import TestCase
+
+from uuidfield.tests.models import (AutoUUIDField, ManualUUIDField,
+    NamespaceUUIDField, BrokenNamespaceUUIDField)
+
+
 class UUIDFieldTestCase(TestCase):
+
     def test_auto_uuid4(self):
-        inst = UUIDFieldTestModel.objects.create()
-        self.assertTrue(inst.uuid)
-        self.assertEquals(len(inst.uuid), 32)
-        # self.assertTrue(isinstance(inst.uuid, uuid.UUID))
+        obj = AutoUUIDField.objects.create()
+        self.assertTrue(obj.uuid)
+        self.assertEquals(len(obj.uuid), 32)
+        self.assertTrue(isinstance(obj.uuid, uuid.UUID))
+        self.assertEquals(obj.uuid.version, 4)
+
+    def test_raises_exception(self):
+        self.assertRaises(IntegrityError, ManualUUIDField.objects.create)
+
+    def test_manual(self):
+        obj = ManualUUIDField.objects.create(uuid=uuid.uuid4())
+        self.assertTrue(obj)
+        self.assertEquals(len(obj.uuid), 32)
+        self.assertTrue(isinstance(obj.uuid, uuid.UUID))
+        self.assertEquals(obj.uuid.version, 4)
+
+    def test_namespace(self):
+        obj = NamespaceUUIDField.objects.create()
+        self.assertTrue(obj)
+        self.assertEquals(len(obj.uuid), 32)
+        self.assertTrue(isinstance(obj.uuid, uuid.UUID))
+        self.assertEquals(obj.uuid.version, 5)
+
+    def test_broken_namespace(self):
+        self.assertRaises(ValueError, BrokenNamespaceUUIDField.objects.create)
+
