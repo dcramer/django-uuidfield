3f24b1d87a247904a849fc97e81ea9123fce2da9
vitaly omelchuk <vitaly.omelchuk@gmail.com>
2014-02-17 11:15:40 +0200

    python 3.3 support fix

--- uuidfield/__init__.py
+++ uuidfield/__init__.py
@@ -1,7 +1,7 @@
 try:
     VERSION = __import__('pkg_resources') \
         .get_distribution('django-uuidfield').version
-except Exception, e:
+except Exception as e:
     VERSION = 'unknown'
     
 from fields import UUIDField
